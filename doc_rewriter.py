297ca7005a77bfce128a9b3205353334afbc0db0
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2025-03-28 23:24:10 +0100

    initial commit

---
+++ doc_rewriter.py
@@ -0,0 +1,109 @@
+def rewrite_document(src):
+    src, packages = find_and_remove_packages(src)
+    src = fix_sections(src)
+    src = remove_commands(src, [r"\documentclass{article}",
+        r"\begin{document}", r"\end{document}"])
+
+    return src, packages
+
+def find_and_remove_packages(src):
+    packages = []
+    offset = 0
+
+    indexed_commands = find_indexed_commands(src, ["usepackage"])
+    for index, _ in indexed_commands:
+        index += offset
+        next_command_start = src.index("\\", index+1)
+
+        packages.append(parse_package(src[index:next_command_start]))
+
+        command_length = next_command_start - index
+        src, offset_change = replace_text("",
+                index, command_length, src)
+
+        offset += offset_change
+
+    return src, packages
+
+def parse_package(src):
+    name_start_index = src.index("{")
+    name_end_index = src.index("}")
+    name = src[name_start_index+1:name_end_index] 
+
+    return {
+        "name": name,
+        "line": src.strip()
+    }
+
+def fix_sections(src):
+    """
+    moves section hierarchy down by two levels
+    because theres expected to be two levels of documents
+    above each document
+    """
+    
+    relevant_commands = [
+        "section",
+        "subsection"
+    ]
+
+    offset = 0
+    indexed_commands = find_indexed_commands(src, relevant_commands)
+    for index, command in indexed_commands:
+        index += offset
+        command_length = len(command)
+
+        replacement_command = get_replacement_command(src, index, command)
+
+        new_command_call = f"\n\\{replacement_command}"
+        src, offset_change = replace_text(new_command_call,
+                index, command_length+1, src)
+
+        offset += offset_change
+
+    return src
+
+def remove_commands(src, commands):
+    for command in commands:
+        src = src.replace(command, "")
+    return src
+
+def replace_text(new_text, start_index, length, src):
+    """replace text at a specific index, while being aware of the offset this causes"""
+
+    end_index = start_index + length
+
+    src = src[:start_index] + new_text + src[end_index:]
+    
+    return src, len(new_text) - length
+
+def get_replacement_command(src, index, command):
+    if command == "section":
+        return "documentsection"
+    if command == "subsection":
+        return "documentsubsection"
+
+def find_indexed_commands(src, commands):
+    found_commands = []
+    index = 0
+
+    while True:
+        found_index = None
+        found_command = None
+        for command in commands:
+            for suffix in ["[", "{", "*[", "*{"]:
+                effective_command = "\\"+command+suffix
+                if effective_command not in src[index+1:]:
+                    continue
+                command_index = src.index(effective_command, index+1)
+                if found_index is None or command_index < found_index:
+                    found_index = command_index
+                    found_command = command
+
+        if found_index is None:
+            break
+
+        index = found_index
+        found_commands.append((index, found_command))
+
+    return found_commands
---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import hashlib
+import doc_rewriter
+
+title = "Example Document"
+author = "John Doe"
+date = "Today"
+
+documents = [
+    "funktionen/main.tex",
+    "vektoren/main.tex"
+]
+
+def rewrite_doc(doc_path):
+    input_file = os.path.join("input", doc_path)
+
+    md5_hash = hashlib.md5(doc_path.encode("UTF8")).hexdigest()
+    file_name = f"{md5_hash}.tex"
+    output_file = os.path.join("output", file_name)
+
+    with open(input_file, "r") as f:
+        src = f.read()
+
+    # if this becomes too slow make a package cache to read from
+    output_src, packages = doc_rewriter.rewrite_document(src)
+
+    if (os.path.exists(output_file) and
+            os.path.getmtime(input_file) > os.path.getmtime(output_file)):
+        return md5_hash, packages
+
+    with open(output_file, "w") as f:
+        f.write(output_src)
+
+    return md5_hash, packages
+
+def rewrite_docs():
+    doc_file_names = []
+    total_packages = {}
+
+    for document in documents:
+        name, packages = rewrite_doc(document)
+        doc_file_names.append(name)
+
+        for package in packages:
+            name = package["name"]
+            line = package["line"]
+            if name in total_packages:
+                if line != total_packages[name]:
+                    print(f"bad package {name}")
+                    exit(1)
+            else:
+                total_packages[name] = line
+
+    return doc_file_names, total_packages
+
+def rewrite_main(doc_file_names, packages):
+    dir = os.path.dirname(__file__)
+    with open(os.path.join(dir, "template.tex")) as f:
+        src = f.read()
+
+    src = src.replace("\$title\$", title)
+    src = src.replace("\$author\$", author)
+    src = src.replace("\$date\$", date)
+
+    package_imports = ""
+    for package_line in packages.values():
+        package_imports += f"{package_line}\n"
+
+    src = src.replace("\$package_imports\$", package_imports)
+
+    contents = ""
+    for file_name in doc_file_names:
+        contents += "\include{"+file_name+"}\n"
+    src = src.replace("\$contents\$", contents)
+
+    with open("output/main.tex", "w") as f:
+        f.write(src)
+
+doc_file_names, packages = rewrite_docs()
+rewrite_main(doc_file_names, packages)
